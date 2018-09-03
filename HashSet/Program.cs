3946b9af0c0bda6d8d964b0152adf46f981961f3
DAVID HO <davidhorph@hotmail.com>
2018-09-03 02:46:17 -0500

    Practice Hash Set.

--- HashSet/Program.cs
+++ HashSet/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace HashSet
 {
@@ -6,7 +9,50 @@ namespace HashSet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> myHashSet = new HashSet<string>();
+            myHashSet.Add("Hello");
+            myHashSet.Add("World.");
+            myHashSet.Add("How");
+            myHashSet.Add("Hello");
+
+            foreach (var item in myHashSet)
+            {
+                Console.WriteLine(item);
+            }
+
+            Car myCar = new Car()
+            {
+                Make = "Toyota",
+                Model = "Camry",
+                Year = 2005
+            };         
+                        
+            Console.WriteLine(myCar);
+
+            Console.ReadLine();
+        }
+    } 
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double Year { get; set; } 
+
+
+
+        public Car(string make, string model, double year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        } 
+
+
+
+        public Car()
+        {
+            HashSet<Car> CarInstance = new HashSet<Car>();
         }
     }
 }
